f0f43a43d85f1add29bbea3cba2ec712d07975fe
Shem <macapobresshem@gmail.com>
2024-04-07 18:34:18 +0800

    set vagrant user

--- setup/tools/apache.sh
+++ setup/tools/apache.sh
@@ -4,5 +4,9 @@
 sudo dnf -y install httpd
 sudo dnf -y install httpd-devel
 
+# Set permission
+sudo sed -i 's/User apache/User vagrant/g' /etc/httpd/conf/httpd.conf
+sudo sed -i 's/Group apache/Group vagrant/g' /etc/httpd/conf/httpd.conf
+
 # Enable and start the httpd service.
 sudo systemctl enable --now httpd.service
